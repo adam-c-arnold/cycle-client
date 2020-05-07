c0b6ec1502053eb8c89915dfc501076bf5fd4ec0
Adam Christopher Arnold <adamchristopherarnold@Adams-MacBook-Pro.local>
2020-05-07 14:15:33 -0400

    client

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,10 +2,10 @@ let APIURL = '';
 
 switch (window.location.hostname) {
   case 'localhost' || '127.0.0.1':
-    APIURL = 'http://localhost:3002/';
+    APIURL = 'http://localhost:3000/';
     break;
   case 'aa-cycle-client':
-    APIURL = 'https://aa-cycle-client.herokuapp.com/';
+    APIURL = 'https://aa-cycle-app-project.herokuapp.com/';
 }
 
 export default APIURL;
